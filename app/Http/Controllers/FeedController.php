135f923e12af0be7a11d7eff88a3e43826d043e3
ludoguenet <ludovicguenet@gmx.com>
2025-01-01 19:47:01 +0100

    fix

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -5,31 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Feed;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Validation\Rules\File;
 
 class FeedController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return Feed::with('user', 'attachments')->latest()->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -47,44 +32,12 @@ class FeedController extends Controller
 
         if (! empty($validated['attachments'])) {
             $attachments = collect($validated['attachments'])
-                ->map(fn ($attachment) => ['path' => $attachment->store('attachments', 'public')])
+                ->map(static fn (UploadedFile $attachment) => ['path' => $attachment->store('attachments', 'public')])
                 ->all();
 
             $feed->attachments()->createMany($attachments);
         };
 
-        return back()->with('feeds', Feed::with('user', 'attachments')->latest()->get());
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return back();
     }
 }
